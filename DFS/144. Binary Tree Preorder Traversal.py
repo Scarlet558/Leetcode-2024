40979c8c5be0e286af225cae9f0dd2e25ceebe02
Scarlet558 <manasakumbashi@gmail.com>
2024-11-19 00:24:02 -0800

    Nov 15

---
+++ DFS/144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,46 @@
+"""
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        stack = [root]
+        res = []
+
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+
+            if cur.right :
+                stack.append(cur.right)
+            if cur.left :
+                stack.append(cur.left)
+
+        return res
---
+++ DFS/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,66 @@
+"""
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+"""
+from logging import root
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        def inorder(root):
+            if not root:
+                return
+
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res
+
+# Create tree manually for testing in PyCharm
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(8)
+root.left.right.left = TreeNode(6)
+root.left.right.right = TreeNode(7)
+root.right.right.left = TreeNode(9)
+
+# Test the solution
+sol = Solution()
+print(sol.inorderTraversal(root))
+
